b488acd8d6a9c4b9abca545bd1ae503b456c723a
unknown <aja.washington13@gmail.com>
2020-04-13 13:51:55 -0500

    owner

--- DogWalkerMVC/Models/Owner.cs
+++ DogWalkerMVC/Models/Owner.cs
@@ -8,5 +8,12 @@ namespace DogWalkerMVC.Models
     public class Owner
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int NeighborhoodId { get; set; }
+
+        public string Phone { get; set; }
     }
 }
