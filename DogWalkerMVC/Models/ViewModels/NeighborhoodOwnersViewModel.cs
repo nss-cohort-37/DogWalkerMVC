445fbc513bc9279b5cce0a46c50b8f5962254eae
namitamanohar <manohar.namita@gmail.com>
2020-04-13 13:08:07 -0500

    owners tab and adding owner no styling

---
+++ DogWalkerMVC/Models/ViewModels/NeighborhoodOwnersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogWalkerMVC.Models.ViewModels
+{
+    public class NeighborhoodOwnersViewModel
+    {
+        public string NeighborhoodName { get; set; }
+
+        public int OwnerCount { get; }
+
+        public List<OwnerViewModel> OwnerViewModels { get; set; }
+    }
+}
